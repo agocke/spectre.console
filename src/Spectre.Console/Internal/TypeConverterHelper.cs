4192c7bd574a481f88e528941ae37160fe937353
Andy Gocke <andy@commentout.net>
2024-04-12 14:03:53 -0700

    Only use intrinsics

--- src/Spectre.Console/Internal/TypeConverterHelper.cs
+++ src/Spectre.Console/Internal/TypeConverterHelper.cs
@@ -2,8 +2,8 @@ namespace Spectre.Console;
 
 internal static class TypeConverterHelper
 {
-    internal static bool AreGenericTypeConvertersSupported =>
-        !AppContext.TryGetSwitch("Spectre.Console.TypeConverterHelper.AreGenericTypeConvertersSupported", out var enabled) || enabled;
+    internal static bool IsGetConverterSupported =>
+        !AppContext.TryGetSwitch("Spectre.Console.TypeConverterHelper.IsGetConverterSupported ", out var enabled) || enabled;
 
     public static string ConvertToString<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>(T input)
     {
@@ -55,7 +55,7 @@ internal static class TypeConverterHelper
 
     public static TypeConverter GetTypeConverter<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>()
     {
-        var converter = GetCheckedConverter();
+        var converter = GetConverter();
         if (converter != null)
         {
             return converter;
@@ -77,16 +77,75 @@ internal static class TypeConverterHelper
 
         throw new InvalidOperationException("Could not find type converter");
 
-        [UnconditionalSuppressMessage("Trimming", "IL2026:RequiresUnreferencedCode", Justification = "TypeConverter is only used for non-generic types.")]
-        static TypeConverter? GetCheckedConverter()
+        static TypeConverter? GetConverter()
         {
-            if (!AreGenericTypeConvertersSupported && typeof(T).IsGenericType)
+            if (!IsGetConverterSupported)
             {
-                throw new InvalidOperationException("Generic type converters are not supported when trimming is enabled.");
+                return GetIntrinsicConverter(typeof(T));
             }
 
             var converter = TypeDescriptor.GetConverter(typeof(T));
             return converter;
         }
+    }
+
+    private static readonly Dictionary<Type, Func<Type, TypeConverter>> s_intrinsicConverters = new()
+    {
+        [typeof(bool)] = _ => new BooleanConverter(),
+        [typeof(byte)] = _ => new ByteConverter(),
+        [typeof(sbyte)] = _ => new SByteConverter(),
+        [typeof(char)] = _ => new CharConverter(),
+        [typeof(double)] = _ => new DoubleConverter(),
+        [typeof(string)] = _ => new StringConverter(),
+        [typeof(int)] = _ => new Int32Converter(),
+        [typeof(Int128)] = _ => new Int128Converter(),
+        [typeof(short)] = _ => new Int16Converter(),
+        [typeof(long)] = _ => new Int64Converter(),
+        [typeof(float)] = _ => new SingleConverter(),
+        [typeof(Half)] = _ => new HalfConverter(),
+        [typeof(UInt128)] = _ => new UInt128Converter(),
+        [typeof(ushort)] = _ => new UInt16Converter(),
+        [typeof(uint)] = _ => new UInt32Converter(),
+        [typeof(ulong)] = _ => new UInt64Converter(),
+        [typeof(object)] = _ => new TypeConverter(),
+        [typeof(CultureInfo)] = _ => new CultureInfoConverter(),
+        [typeof(DateOnly)] = _ => new DateOnlyConverter(),
+        [typeof(DateTime)] = _ => new DateTimeConverter(),
+        [typeof(DateTimeOffset)] = _ => new DateTimeOffsetConverter(),
+        [typeof(decimal)] = _ => new DecimalConverter(),
+        [typeof(TimeOnly)] = _ => new TimeOnlyConverter(),
+        [typeof(TimeSpan)] = _ => new TimeSpanConverter(),
+        [typeof(Guid)] = _ => new GuidConverter(),
+        [typeof(Uri)] = _ => new UriTypeConverter(),
+        [typeof(Version)] = _ => new VersionConverter(),
+
+        [typeof(Array)] = _ => new ArrayConverter(),
+        [typeof(ICollection)] = _ => new CollectionConverter(),
+        [typeof(Enum)] = (type) => new EnumConverter(type),
+    };
+
+    private static TypeConverter? GetIntrinsicConverter(Type type)
+    {
+        if (type.IsArray)
+        {
+            type = typeof(Array);
+        }
+
+        if (type.IsAssignableTo(typeof(ICollection)))
+        {
+            type = typeof(ICollection);
+        }
+
+        if (type.IsEnum)
+        {
+            type = typeof(Enum);
+        }
+
+        if (s_intrinsicConverters.TryGetValue(type, out var factory))
+        {
+            return factory(type);
+        }
+
+        return null;
     }
 }
