0841d807f1abb9e99ff573c76e51fa789502f34b
Andy Gocke <andy@commentout.net>
2024-04-12 14:50:07 -0700

    Simplify annotation

--- src/Spectre.Console/Internal/TypeConverterHelper.cs
+++ src/Spectre.Console/Internal/TypeConverterHelper.cs
@@ -1,11 +1,15 @@
+using DAM = System.Diagnostics.CodeAnalysis.DynamicallyAccessedMembersAttribute;
+
 namespace Spectre.Console;
 
 internal static class TypeConverterHelper
 {
+    private const DynamicallyAccessedMemberTypes ConverterAnnotation = DynamicallyAccessedMemberTypes.PublicParameterlessConstructor | DynamicallyAccessedMemberTypes.PublicFields;
+
     internal static bool IsGetConverterSupported =>
         !AppContext.TryGetSwitch("Spectre.Console.TypeConverterHelper.IsGetConverterSupported ", out var enabled) || enabled;
 
-    public static string ConvertToString<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>(T input)
+    public static string ConvertToString<[DAM(ConverterAnnotation)] T>(T input)
     {
         var result = GetTypeConverter<T>().ConvertToInvariantString(input);
         if (result == null)
@@ -16,7 +20,7 @@ internal static class TypeConverterHelper
         return result;
     }
 
-    public static bool TryConvertFromString<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>(string input, [MaybeNull] out T? result)
+    public static bool TryConvertFromString<[DAM(ConverterAnnotation)] T>(string input, [MaybeNull] out T? result)
     {
         try
         {
@@ -30,8 +34,7 @@ internal static class TypeConverterHelper
         }
     }
 
-    public static bool TryConvertFromStringWithCulture<
-        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>(string input, CultureInfo? info, [MaybeNull] out T? result)
+    public static bool TryConvertFromStringWithCulture<[DAM(ConverterAnnotation)] T>(string input, CultureInfo? info, [MaybeNull] out T? result)
     {
         try
         {
@@ -53,7 +56,7 @@ internal static class TypeConverterHelper
         }
     }
 
-    public static TypeConverter GetTypeConverter<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>()
+    public static TypeConverter GetTypeConverter<[DAM(ConverterAnnotation)] T>()
     {
         var converter = GetConverter();
         if (converter != null)
@@ -77,6 +80,8 @@ internal static class TypeConverterHelper
 
         throw new InvalidOperationException("Could not find type converter");
 
+        [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2087", Justification = "Feature switches are not currently supported in the analyzer")]
+        [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026", Justification = "Feature switches are not currently supported in the analyzer")]
         static TypeConverter? GetConverter()
         {
             if (!IsGetConverterSupported)
@@ -84,47 +89,59 @@ internal static class TypeConverterHelper
                 return GetIntrinsicConverter(typeof(T));
             }
 
-            var converter = TypeDescriptor.GetConverter(typeof(T));
-            return converter;
+            return TypeDescriptor.GetConverter(typeof(T));
         }
     }
 
-    private static readonly Dictionary<Type, Func<Type, TypeConverter>> s_intrinsicConverters = new()
+    private delegate TypeConverter FuncWithDam([DAM(ConverterAnnotation)] Type type);
+
+    private static readonly Dictionary<Type, FuncWithDam> _intrinsicConverters;
+
+    static TypeConverterHelper()
     {
-        [typeof(bool)] = _ => new BooleanConverter(),
-        [typeof(byte)] = _ => new ByteConverter(),
-        [typeof(sbyte)] = _ => new SByteConverter(),
-        [typeof(char)] = _ => new CharConverter(),
-        [typeof(double)] = _ => new DoubleConverter(),
-        [typeof(string)] = _ => new StringConverter(),
-        [typeof(int)] = _ => new Int32Converter(),
-        [typeof(Int128)] = _ => new Int128Converter(),
-        [typeof(short)] = _ => new Int16Converter(),
-        [typeof(long)] = _ => new Int64Converter(),
-        [typeof(float)] = _ => new SingleConverter(),
-        [typeof(Half)] = _ => new HalfConverter(),
-        [typeof(UInt128)] = _ => new UInt128Converter(),
-        [typeof(ushort)] = _ => new UInt16Converter(),
-        [typeof(uint)] = _ => new UInt32Converter(),
-        [typeof(ulong)] = _ => new UInt64Converter(),
-        [typeof(object)] = _ => new TypeConverter(),
-        [typeof(CultureInfo)] = _ => new CultureInfoConverter(),
-        [typeof(DateOnly)] = _ => new DateOnlyConverter(),
-        [typeof(DateTime)] = _ => new DateTimeConverter(),
-        [typeof(DateTimeOffset)] = _ => new DateTimeOffsetConverter(),
-        [typeof(decimal)] = _ => new DecimalConverter(),
-        [typeof(TimeOnly)] = _ => new TimeOnlyConverter(),
-        [typeof(TimeSpan)] = _ => new TimeSpanConverter(),
-        [typeof(Guid)] = _ => new GuidConverter(),
-        [typeof(Uri)] = _ => new UriTypeConverter(),
-        [typeof(Version)] = _ => new VersionConverter(),
+        _intrinsicConverters = new()
+        {
+            [typeof(bool)] = _ => new BooleanConverter(),
+            [typeof(byte)] = _ => new ByteConverter(),
+            [typeof(sbyte)] = _ => new SByteConverter(),
+            [typeof(char)] = _ => new CharConverter(),
+            [typeof(double)] = _ => new DoubleConverter(),
+            [typeof(string)] = _ => new StringConverter(),
+            [typeof(int)] = _ => new Int32Converter(),
+            [typeof(Int128)] = _ => new Int128Converter(),
+            [typeof(short)] = _ => new Int16Converter(),
+            [typeof(long)] = _ => new Int64Converter(),
+            [typeof(float)] = _ => new SingleConverter(),
+            [typeof(Half)] = _ => new HalfConverter(),
+            [typeof(UInt128)] = _ => new UInt128Converter(),
+            [typeof(ushort)] = _ => new UInt16Converter(),
+            [typeof(uint)] = _ => new UInt32Converter(),
+            [typeof(ulong)] = _ => new UInt64Converter(),
+            [typeof(object)] = _ => new TypeConverter(),
+            [typeof(CultureInfo)] = _ => new CultureInfoConverter(),
+            [typeof(DateOnly)] = _ => new DateOnlyConverter(),
+            [typeof(DateTime)] = _ => new DateTimeConverter(),
+            [typeof(DateTimeOffset)] = _ => new DateTimeOffsetConverter(),
+            [typeof(decimal)] = _ => new DecimalConverter(),
+            [typeof(TimeOnly)] = _ => new TimeOnlyConverter(),
+            [typeof(TimeSpan)] = _ => new TimeSpanConverter(),
+            [typeof(Guid)] = _ => new GuidConverter(),
+            [typeof(Uri)] = _ => new UriTypeConverter(),
+            [typeof(Version)] = _ => new VersionConverter(),
 
-        [typeof(Array)] = _ => new ArrayConverter(),
-        [typeof(ICollection)] = _ => new CollectionConverter(),
-        [typeof(Enum)] = (type) => new EnumConverter(type),
-    };
+            [typeof(Array)] = _ => new ArrayConverter(),
+            [typeof(ICollection)] = _ => new CollectionConverter(),
+            [typeof(Enum)] = CreateEnumConverter(),
+        };
+    }
 
-    private static TypeConverter? GetIntrinsicConverter(Type type)
+    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2111", Justification = "Delegate reflection is safe for all usages in this type.")]
+    private static FuncWithDam CreateEnumConverter() => ([DAM(ConverterAnnotation)] Type type) => new EnumConverter(type);
+
+    /// <summary>
+    /// A highly-constrained version of <see cref="TypeDescriptor.GetConverter(Type)" /> that only returns intrinsic converters.
+    /// </summary>
+    private static TypeConverter? GetIntrinsicConverter([DAM(ConverterAnnotation)] Type type)
     {
         if (type.IsArray)
         {
@@ -141,7 +158,7 @@ internal static class TypeConverterHelper
             type = typeof(Enum);
         }
 
-        if (s_intrinsicConverters.TryGetValue(type, out var factory))
+        if (_intrinsicConverters.TryGetValue(type, out var factory))
         {
             return factory(type);
         }
